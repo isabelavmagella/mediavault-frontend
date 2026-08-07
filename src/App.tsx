ffebc21711d83d0d9fb3fddb310d29803d5a1f83
Isabela Virginio Magella <isabelamagella@gmail.com>
2026-08-07 09:22:04 -0300

    feat(router): adicionar roteamento na aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,28 @@
+import { Link, Route, Routes } from "react-router";
+
 function App() {
   return (
-    <>
-      
-    </>
-  )
+    <Routes>
+      <Route
+        path="/login"
+        element={
+          <div>
+            <h1>Página de Login</h1>
+            <Link to="/register">Ir a Registro</Link>
+          </div>
+        }
+      />
+      <Route
+        path="/register"
+        element={
+          <div>
+            <h1>Página de Registro</h1>
+            <Link to="/login">Ir a Login</Link>
+          </div>
+        }
+      />
+    </Routes>
+  );
 }
 
-export default App
+export default App;
