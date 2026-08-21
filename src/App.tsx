53d7006499f41aa5827a21f2cd1d08af4b008df6
Isabela Virginio Magella <isabelamagella@gmail.com>
2026-08-21 10:32:57 -0300

    feat(router): integrar AuthProvider e pagina de Login no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,23 @@
 import { Link, Route, Routes } from "react-router";
+import { Login } from "./pages/Login/Login";
+import { AuthProvider } from "./context/AuthContext";
 
 function App() {
   return (
-    <Routes>
-      <Route
-        path="/login"
-        element={
-          <div>
-            <h1>Página de Login</h1>
-            <Link to="/register">Ir a Registro</Link>
-          </div>
-        }
-      />
-      <Route
-        path="/register"
-        element={
-          <div>
-            <h1>Página de Registro</h1>
-            <Link to="/login">Ir a Login</Link>
-          </div>
-        }
-      />
-    </Routes>
+    <AuthProvider>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route
+          path="/register"
+          element={
+            <div>
+              <h1>Página de Registro</h1>
+              <Link to="/login">Ir a Login</Link>
+            </div>
+          }
+        />
+      </Routes>
+    </AuthProvider>
   );
 }
 
