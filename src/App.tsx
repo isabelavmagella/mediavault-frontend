8011ca6a0549740b0ada283e8ec40e25651ab14a
Isabela Virginio Magella <isabelamagella@gmail.com>
2026-08-21 12:57:24 -0300

    feat(router): usar pagina de Registro real no App

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,14 @@
-import { Link, Route, Routes } from "react-router";
+import { Route, Routes } from "react-router";
 import { Login } from "./pages/Login/Login";
 import { AuthProvider } from "./context/AuthContext";
+import { Register } from "./pages/Register/Register";
 
 function App() {
   return (
     <AuthProvider>
       <Routes>
         <Route path="/login" element={<Login />} />
-        <Route
-          path="/register"
-          element={
-            <div>
-              <h1>Página de Registro</h1>
-              <Link to="/login">Ir a Login</Link>
-            </div>
-          }
-        />
+        <Route path="/register" element={<Register />} />
       </Routes>
     </AuthProvider>
   );
