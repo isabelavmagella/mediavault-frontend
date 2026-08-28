eac0b56986704ce73f16d81453503c4fe9073efb
Isabela Virginio Magella <isabelamagella@gmail.com>
2026-08-28 08:36:43 -0300

    feat(router): integrar Dashboard e layout protegido no App

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,9 @@ import { Route, Routes } from "react-router";
 import { Login } from "./pages/Login/Login";
 import { AuthProvider } from "./context/AuthContext";
 import { Register } from "./pages/Register/Register";
+import { Dashboard } from "./pages/Dashboard/Dashboard";
+import { ProtectedRoute } from "./routes/ProtectedRoute";
+import { Appshell } from "./components/Sidebar/Appshell";
 
 function App() {
   return (
@@ -9,6 +12,11 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route element={<ProtectedRoute />}>
+          <Route element={<Appshell />}>
+            <Route path="/" element={<Dashboard />} />
+          </Route>
+        </Route>
       </Routes>
     </AuthProvider>
   );
